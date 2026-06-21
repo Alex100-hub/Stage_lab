6cd0b55959f0eb1449cdff28e665fb51e5385ac0
Alex100-hub <deero6677@gmail.com>
2026-06-21 13:52:28 +0300

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'stagelab-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
+
